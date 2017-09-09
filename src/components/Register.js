4770c53ce260fb78b7eda4dbd850e6683d86b6f1
honmanyau <honmanyau@users.noreply.github.com>
2017-09-09 22:34:53 +1000

    push

--- src/components/Register.js
+++ src/components/Register.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
-import { Redirect } from 'react-router-dom';
 
 import * as AuthActions from '../actions/auth';
 
